89c0f9fa21d8324e508b563c3fa5e117044e835b
Gervig <gervig91@gmail.com>
2025-03-25 23:40:51 +0100

    api fetch testing

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,16 +1,30 @@
 package app;
 
+import app.callable.DetailsServiceCallable;
 import app.config.HibernateConfig;
+import app.dtos.MovieDTO;
 import app.rest.ApplicationConfig;
 import app.rest.Routes;
+import app.services.Service;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
 
 public class Main
 {
     public static void main(String[] args)
     {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
+
+        List<String> movieApiIds = Service.getMovieApiIds();
+
+        movieApiIds.forEach(System.out::println);
+
+//        List<MovieDTO> movieDTOS = DetailsServiceCallable.getMovieDTOs(movieApiIds);
+//
+//        movieDTOS.forEach(System.out::println);
 
 //        ApplicationConfig
 //                .getInstance()
