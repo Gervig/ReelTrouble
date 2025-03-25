dae6b37579389f651c6164111f9e5a32dab4efc8
Gervig <gervig91@gmail.com>
2025-03-25 10:01:48 +0100

    Create Actor.java

---
+++ src/main/java/app/entities/Actor.java
@@ -0,0 +1,28 @@
+package app.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
+import java.util.Set;
+
+@DynamicUpdate
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@ToString
+@Entity
+public class Actor
+{
+    // basic
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "actor_api_id")
+    private Long actorApiId;
+    private String name;
+
+    @Setter
+    @ManyToMany(mappedBy = "actors")
+    private Set<Media> movies;
+}
