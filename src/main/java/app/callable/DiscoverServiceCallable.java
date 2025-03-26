39b80ad766a16f39238c20b8c4ce936f7144fcaf
Gervig <gervig91@gmail.com>
2025-03-26 09:22:16 +0100

    threads

--- src/main/java/app/callable/DiscoverServiceCallable.java
+++ src/main/java/app/callable/DiscoverServiceCallable.java
@@ -28,7 +28,7 @@ public class DiscoverServiceCallable implements Callable<List<String>>
     public static List<String> getMovieApiIds(int totalPages)
     {
         List<Future<List<String>>> futureList = new ArrayList<>();
-        ExecutorService executorService = Executors.newFixedThreadPool(10); // increase for faster performance, beware of overload
+        ExecutorService executorService = Executors.newFixedThreadPool(20); // increase for faster performance, beware of overload
 
         for (int i = 1; i <= totalPages; i++)
         {
