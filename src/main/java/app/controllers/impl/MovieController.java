37069bea80a61371c79f6704a9fc81e075ed50d5
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-03-26 10:43:38 +0100

    Lavet metoder der kan finde en random film ud fra en genre og en ud fra en genre som IKKE fremgår af brugerens liste

---
+++ src/main/java/app/controllers/impl/MovieController.java
@@ -0,0 +1,69 @@
+package app.controllers.impl;
+
+import app.controllers.IController;
+import app.daos.impl.MovieDAO;
+import app.dtos.MovieDTO;
+import app.entities.Genre;
+import app.entities.Movie;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
+import java.util.Random;
+
+public class MovieController implements IController<MovieDTO, Long>
+{
+    // attributes
+    private static EntityManagerFactory emf;
+    private MovieDAO movieDAO;
+
+    // constructor
+    public MovieController(EntityManagerFactory _emf)
+    {
+        if (emf == null)
+        {
+            emf = _emf;
+        }
+        this.movieDAO = MovieDAO.getInstance(emf);
+    }
+
+    @Override
+    public List<MovieDTO> getAll()
+    {
+        List<Movie> movie = movieDAO.readAll();
+        List<MovieDTO> movieDTOS = movie.stream()
+                .map(MovieDTO::new)
+                .toList();
+        return movieDTOS;
+    }
+
+    @Override
+    public MovieDTO getById(Long id)
+    {
+        Movie movie = movieDAO.read(id);
+        if (movie == null)
+        {
+            return null;
+        }
+        MovieDTO movieDTO = new MovieDTO(movie);
+        return movieDTO;
+    }
+
+    public MovieDTO getRandomMovieInGenre(String genre)
+    {
+        List<Movie> movies = movieDAO.findMoviesByGenre(genre);
+
+        Movie movie = movies.get(new Random().nextInt(movies.size()));
+
+        MovieDTO movieDTO = new MovieDTO(movie);
+        return movieDTO;
+    }
+
+    public MovieDTO getRandomMovieExclUsersList(String genre, Long userID)
+    {
+        List<Movie> movies = movieDAO.findMovieExclUsersList(genre, userID);
+        Movie movie = movies.get(new Random().nextInt(movies.size()));
+
+        MovieDTO movieDTO = new MovieDTO(movie);
+        return movieDTO;
+    }
+}
