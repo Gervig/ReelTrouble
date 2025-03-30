46959594d5586dbfb068007031eb0e6c54bc5258
Gervig <gervig91@gmail.com>
2025-03-30 14:39:25 +0200

    fallback to initialize db name

--- src/main/java/app/config/HibernateConfig.java
+++ src/main/java/app/config/HibernateConfig.java
@@ -105,6 +105,10 @@ public class HibernateConfig
     private static Properties setDeployedProperties(Properties props)
     {
         String DBName = System.getenv("DB_NAME_REEL_TROUBLE");
+        if (DBName == null) {
+            System.out.println("WARNING: DB_NAME_REEL_TROUBLE is not set! Using default value.");
+            DBName = "reel_trouble";
+        }
         props.setProperty("hibernate.connection.url", System.getenv("CONNECTION_STR") + DBName);
         props.setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"));
         props.setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"));
